fd46ad3b1e2fa2edfdaee4bec9c61365dfa3069e
zm <zhangmeng43@gmail.com>
2026-02-20 15:45:31 +0100

    fix(js): ensure updateVehicleMarkers defined before API module uses it

--- js/main.js
+++ js/main.js
@@ -663,23 +663,6 @@ import { installAxiosInterceptors, createBvgApi } from './api.js';
 
         const visibleStopovers = computed(() => currentTripStopovers.value.slice(0, stopoverLimit.value));
 
-        const { fetchDepartures, fetchRadar } = createBvgApi({
-          axios,
-          apiBase,
-          duration,
-          watchedStations,
-          departuresRaw,
-          loading,
-          startRadarLoop,
-          getBoundingBox,
-          radarAbortControllerBox,
-          isRadarActive,
-          radarError,
-          lastRadarData,
-          showMap,
-          updateVehicleMarkers,
-        });
-
         const updateVehicleMarkers = (vehicles) => {
             if (!map) return;
             const activeIds = new Set();
@@ -762,6 +745,25 @@ import { installAxiosInterceptors, createBvgApi } from './api.js';
                 }
             }
         };
+
+
+
+        const { fetchDepartures, fetchRadar } = createBvgApi({
+          axios,
+          apiBase,
+          duration,
+          watchedStations,
+          departuresRaw,
+          loading,
+          startRadarLoop,
+          getBoundingBox,
+          radarAbortControllerBox,
+          isRadarActive,
+          radarError,
+          lastRadarData,
+          showMap,
+          updateVehicleMarkers,
+        });
 
         const startRadarLoop = () => {
             if (radarInterval) clearInterval(radarInterval);
